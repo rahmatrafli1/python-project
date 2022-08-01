c17ae07167a8ea91d64b8419f489c314fe64fa62
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-01 14:19:15 +0000

    add keliling_segitiga.py

---
+++ keliling_segitiga.py
@@ -0,0 +1,9 @@
+print("PROGRAM MENGHITUNG KELILING SEGITIGA")
+
+a = float(input("\nMasukkan nilai a: "))
+b = float(input("Masukkan nilai b: "))
+c = float(input("Masukkan nilai c: "))
+
+keliling = a + b + c
+
+print("\nKeliling segitiga adalah %0.2f" % keliling)

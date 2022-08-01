e532cd1ed5fb1e1080315b38fbddae8f4fd0df38
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-01 21:49:27 +0700

    add lingkaran.py

---
+++ Lingkaran/lingkaran.py
@@ -0,0 +1,9 @@
+print("PROGRAM MENGHITUNG LUAS DAN KELILING LINGKARAN")
+
+r = float(input("\nMasukkan Jari-jari: "))
+
+luas = (22/7) * r * r
+
+keliling = 2 * (22/7) * r
+
+print("\nMaka luas lingkaran %0.2f" % luas , "cm2 dan keliling lingkaran %0.2f" %  keliling , "cm")

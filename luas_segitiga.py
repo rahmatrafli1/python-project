71ae1fff93a29e234df71d0b3968f01c385ed526
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-01 14:08:18 +0000

    init python project

---
+++ luas_segitiga.py
@@ -0,0 +1,8 @@
+print("PROGRAM MENGHITUNG LUAS SEGITIGA")
+
+a = float(input("\nMasukkan alas: "))
+t = float(input("Masukkan tinggi: "))
+
+luas = 0.5*a*t
+
+print("\nLuas Segitiga adalah %0.2f" % luas)

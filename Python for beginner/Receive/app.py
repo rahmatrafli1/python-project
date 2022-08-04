05973c7621aae20d79a02655f915c4e430efbb33
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-04 13:32:59 +0700

    change Receive

--- Python for beginner/Receive/app.py
+++ Python for beginner/Receive/app.py
@@ -1,9 +1,9 @@
 first_name = input("First name: ")
 last_name = input("Last name: ")
-age = int(input("Age: "))
 birth_day = int(input("Enter your birth day: "))
 birth_month = input("Enter your birth month: ")
 birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
 
 print("Hello I am " + first_name, last_name +
-      ".", "I am age", age, "years old.", "I am birth", birth_day, birth_month, birth_year + ".")
+      ".", "I am birth", birth_day, birth_month, birth_year + ".", "I am age", age, "years old.")

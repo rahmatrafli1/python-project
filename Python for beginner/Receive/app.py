cdb3d0ee8e330ed8adcd204af4936581ba344a05
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-04 13:15:57 +0700

    change Receive

--- Python for beginner/Receive/app.py
+++ Python for beginner/Receive/app.py
@@ -2,4 +2,5 @@ first_name = input("First name: ")
 last_name = input("Last name: ")
 age = int(input("Age: "))
 
-print("Hello I am", first_name, last_name + ".", "I am age", age, "years old.")
+print("Hello I am " + first_name, last_name +
+      ".", "I am age", age, "years old.")

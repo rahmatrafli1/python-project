4cba167117bf59e7d77ffd443647247c077ee9d3
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-08-04 13:37:18 +0700

    change HelloWorld

--- Python for beginner/HelloWorld/app.py
+++ Python for beginner/HelloWorld/app.py
@@ -1,5 +1,5 @@
 name = "Rahmat Rafli"
 age = 24
-price = 25000
+price = "25.000"
 print("Hello World! I am", name + ".", "I am age", age, "years old.")
-print("Price certificate is Rp.", price, ",-")
+print("Price certificate is Rp.", price + ",-")
